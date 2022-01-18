ecb973b0a3190eb7f4793ac791272c7bf1ccff88
DEV VRAT <devvrat709@gmail.com>
2022-01-18 10:50:41 +0530

    commit

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,12 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import videojs from 'video.js';
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.css'],
 })
 export class VideoPlayerComponent implements OnInit {
-  constructor() {}
+  @ViewChild('target', { static: true }) target: ElementRef;
+  @Input() options: {
+    fluid: boolean;
+    aspectRatio: string;
+    autoplay: boolean;
+    sources: {
+      src: string;
+      type: string;
+    }[];
+  };
+  player: videojs.Player;
 
-  ngOnInit(): void {}
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    // instantiate Video.js
+    this.player = videojs(
+      this.target.nativeElement,
+      this.options,
+      function onPlayerReady() {
+        console.log('onPlayerReady', this);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    // destroy player
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
 }
